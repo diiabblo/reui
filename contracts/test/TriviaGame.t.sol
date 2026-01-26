1274b75ec674aa5a1d12ebea9f7fd987ba865112
diiabblo <solsticeveil46@gmail.com>
2026-01-26 19:07:27 +0100

    Update TriviaGame tests to use SimpleTriviaGame contract

--- contracts/test/TriviaGame.t.sol
+++ contracts/test/TriviaGame.t.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../src/TriviaGame.sol";
+import "../src/SimpleTriviaGame.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+contract MockERC20 is ERC20 {
+    constructor() ERC20("Mock cUSD", "mcUSD") {
+        _mint(msg.sender, 1000000 * 10**18);
+    }
+}
+
 contract TriviaGameTest is Test {
-    TriviaGame public triviaGame;
+    SimpleTriviaGame public triviaGame;
     MockERC20 public mockCUSD;
     
     address public owner = address(0x1);
@@ -17,20 +23,16 @@ contract TriviaGameTest is Test {
     function setUp() public {
         vm.startPrank(owner);
         mockCUSD = new MockERC20();
-        triviaGame = new TriviaGame(address(mockCUSD));
-        
-        // Create a game
-        triviaGame.createGame("Test Game", 3);
+        triviaGame = new SimpleTriviaGame(address(mockCUSD));
         
         // Fund players with cUSD
-        uint256 playerFunds = 10 * 10**18; // 10 cUSD each
+        uint256 playerFunds = 100 * 10**18;
         mockCUSD.transfer(player1, playerFunds);
         mockCUSD.transfer(player2, playerFunds);
         mockCUSD.transfer(player3, playerFunds);
+        vm.stopPrank();
         
         // Approve the game contract to spend players' tokens
-        vm.stopPrank();
-        
         vm.startPrank(player1);
         mockCUSD.approve(address(triviaGame), type(uint256).max);
         vm.stopPrank();
@@ -44,101 +46,107 @@ contract TriviaGameTest is Test {
         vm.stopPrank();
     }
     
-    function test_CreateGame() public view {
-        // Game 1 was created in setUp
-        assertEq(triviaGame.getGameId(1), 1);
-        assertEq(triviaGame.getGameEntryFee(1), 0.1 * 10**18);
-        assertEq(triviaGame.getGamePrizePool(1), 0);
-        assertEq(triviaGame.getGameMaxPlayers(1), 3);
-        assertEq(uint256(triviaGame.getGameState(1)), 0); // GameState.Open
-    }
-    
-    function test_JoinGame() public {
-        // Player1 joins game 1
-        vm.startPrank(player1);
-        triviaGame.joinGame(1);
+    function test_AddQuestion() public {
+        string[] memory options = new string[](4);
+        options[0] = "Option A";
+        options[1] = "Option B";
+        options[2] = "Option C";
+        options[3] = "Option D";
         
-        // Check that player1 is in the game
-        address[] memory players = triviaGame.getPlayers(1);
-        assertEq(players.length, 1);
-        assertEq(players[0], player1);
-        
-        // Check that player1's balance decreased by the entry fee
-        assertEq(mockCUSD.balanceOf(player1), (10 * 10**18) - (0.1 * 10**18));
-        
-        // Check that the prize pool increased
-        assertEq(triviaGame.getGamePrizePool(1), 0.1 * 10**18);
-    }
-    
-    function test_StartAndCompleteGame() public {
-        // Players join the game
-        vm.startPrank(player1);
-        triviaGame.joinGame(1);
-        vm.stopPrank();
-        
-        vm.startPrank(player2);
-        triviaGame.joinGame(1);
-        vm.stopPrank();
-        
-        vm.startPrank(player3);
-        triviaGame.joinGame(1);
-        vm.stopPrank();
-        
-        // Start the game (as owner)
-        vm.startPrank(owner);
-        triviaGame.startGame(1);
-        
-        // Complete the game with winners
-        address[] memory winners = new address[](3);
-        winners[0] = player1; // 1st place
-        winners[1] = player2; // 2nd place
-        winners[2] = player3; // 3rd place
-        
-        triviaGame.completeGame(1, winners);
-        
-        // Check game state
-        assertEq(uint256(triviaGame.getGameState(1)), 2); // GameState.Completed
-        
-        // Check winners
-        address[] memory gameWinners = triviaGame.getWinners(1);
-        assertEq(gameWinners.length, 3);
-        assertEq(gameWinners[0], player1);
-        assertEq(gameWinners[1], player2);
-        assertEq(gameWinners[2], player3);
-        
-        // Check prize distribution (80% to 1st, 15% to 2nd, 5% to 3rd)
-        uint256 totalPrize = 3 * 0.1 * 10**18; // 3 players * 0.1 cUSD each
-        uint256 firstPrize = (totalPrize * 80) / 100;
-        uint256 secondPrize = (totalPrize * 15) / 100;
-        uint256 thirdPrize = (totalPrize * 5) / 100;
-        
-        // Check player balances
-        assertEq(mockCUSD.balanceOf(player1), (10 * 10**18) - (0.1 * 10**18) + firstPrize);
-        assertEq(mockCUSD.balanceOf(player2), (10 * 10**18) - (0.1 * 10**18) + secondPrize);
-        assertEq(mockCUSD.balanceOf(player3), (10 * 10**18) - (0.1 * 10**18) + thirdPrize);
-    }
-    
-    function test_CancelGame() public {
-        // Get initial balance before joining
-        uint256 initialBalance = mockCUSD.balanceOf(player1);
-        
-        // Player1 joins the game
-        vm.startPrank(player1);
-        triviaGame.joinGame(1);
-        vm.stopPrank();
-        
-        // Cancel the game (as owner)
         vm.prank(owner);
-        triviaGame.cancelGame(1);
+        triviaGame.addQuestion(
+            "What is Celo?",
+            options,
+            1,
+            1 * 10**18,
+            SimpleTriviaGame.Category.Celo,
+            SimpleTriviaGame.Difficulty.Easy
+        );
         
-        // Check that player1 got their entry fee back
-        uint256 finalBalance = mockCUSD.balanceOf(player1);
-        assertEq(finalBalance, initialBalance);
-        
-        // Check game state
-        assertEq(uint256(triviaGame.getGameState(1)), 3); // GameState.Cancelled
+        assertEq(triviaGame.questionId(), 1);
     }
-}
+    
+    function test_RevertWhen_InvalidOptions() public {
+        string[] memory options = new string[](1);
+        options[0] = "Only one option";
+        
+        vm.prank(owner);
+        vm.expectRevert(SimpleTriviaGame.InvalidOptions.selector);
+        triviaGame.addQuestion(
+            "Test Question",
+            options,
+            0,
+            1 * 10**18,
+            SimpleTriviaGame.Category.Celo,
+            SimpleTriviaGame.Difficulty.Easy
+        );
+    }
+    
+    function test_RevertWhen_InvalidCorrectOption() public {
+        string[] memory options = new string[](4);
+        options[0] = "Option A";
+        options[1] = "Option B";
+        options[2] = "Option C";
+        options[3] = "Option D";
+        
+        vm.prank(owner);
+        vm.expectRevert(SimpleTriviaGame.InvalidCorrectOption.selector);
+        triviaGame.addQuestion(
+            "Test Question",
+            options,
+            5, // Invalid index
+            1 * 10**18,
+            SimpleTriviaGame.Category.Celo,
+            SimpleTriviaGame.Difficulty.Easy
+        );
+    }
+    
+    function test_DeactivateQuestion() public {
+        string[] memory options = new string[](4);
+        options[0] = "Option A";
+        options[1] = "Option B";
+        options[2] = "Option C";
+        options[3] = "Option D";
+        
+        vm.startPrank(owner);
+        triviaGame.addQuestion(
+            "Test Question",
+            options,
+            0,
+            1 * 10**18,
+            SimpleTriviaGame.Category.Celo,
+            SimpleTriviaGame.Difficulty.Easy
+        );
+        
+        triviaGame.deactivateQuestion(1);
+        vm.stopPrank();
+    }
+    
+    function test_UserScoresTracking() public {
+        // Initial score should be 0
+        assertEq(triviaGame.userScores(player1), 0);
+    }
+    
+    function test_ContractOwnershipAndPermissions() public {
+        // Verify owner can add questions
+        string[] memory options = new string[](4);
+        options[0] = "A";
+        options[1] = "B";
+        options[2] = "C";
+        options[3] = "D";
+        
+        vm.prank(owner);
+        triviaGame.addQuestion(
+            "Owner Test",
+            options,
+            0,
+            1 * 10**18,
+            SimpleTriviaGame.Category.Celo,
+            SimpleTriviaGame.Difficulty.Easy
+        );
+        
+        assertEq(triviaGame.questionId(), 1);
+    }
 
 contract MockERC20 is ERC20 {
     constructor() ERC20("Mock cUSD", "mcUSD") {
