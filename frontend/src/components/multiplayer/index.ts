ef94082b1242e220b2f116c2bb473ce38b8baeae
diiabblo <solsticeveil46@gmail.com>
2026-02-12 11:13:22 +0100

    feat(multiplayer): add barrel exports for multiplayer components

---
+++ frontend/src/components/multiplayer/index.ts
@@ -0,0 +1,6 @@
+export { RoomCard } from './RoomCard';
+export { RoomList } from './RoomList';
+export { PlayerList } from './PlayerList';
+export { RoomChat } from './RoomChat';
+export { CreateRoomModal } from './CreateRoomModal';
+export { LiveLeaderboard } from './LiveLeaderboard';
