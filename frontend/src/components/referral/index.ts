55b53694af15cf89a4b7f4728344eec39ecfe863
diiabblo <solsticeveil46@gmail.com>
2026-02-13 02:38:21 +0100

    feat(referral): add barrel exports

---
+++ frontend/src/components/referral/index.ts
@@ -0,0 +1,5 @@
+export { ReferralCodeCard } from './ReferralCodeCard';
+export { ReferralList } from './ReferralList';
+export { ReferralStats } from './ReferralStats';
+export { ReferralHowItWorks } from './ReferralHowItWorks';
+export { ReferralInput } from './ReferralInput';
